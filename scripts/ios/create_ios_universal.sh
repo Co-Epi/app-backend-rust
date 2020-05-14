008945846bb03381bf4707af626f042f16351704
Ivan Schuetz <ivanschuetz@gmail.com>
2020-05-14 17:28:12 +0200

    Add script to generate universal library (that actually works on device)

---
+++ scripts/ios/create_ios_universal.sh
@@ -0,0 +1,15 @@
+# Requirements: 
+# 1. https://github.com/getditto/rust-bitcode
+# 2. libtool
+# See https://github.com/Co-Epi/app-backend-rust/wiki/Building-library-for-iOS
+
+cd ../..
+
+RUSTFLAGS="-Z embed-bitcode" cargo +ios-arm64 build --target aarch64-apple-ios --release --lib
+cargo build --target=x86_64-apple-ios
+
+libtool -static -o ./target/libtcn_client.a ./target/aarch64-apple-ios/release/libtcn_client.a ./target/x86_64-apple-ios/release/libtcn_client.a
+
+# Copy lib into iOS app
+# mv ./target/libtcn_client.a <path to iOS app's root dir>
+
