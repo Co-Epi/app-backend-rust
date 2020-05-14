d764a47f31f78e1d04912b146c0ade9fde75f3ee
Ivan Schuetz <ivanschuetz@gmail.com>
2020-05-14 21:32:05 +0200

    Fix script

--- scripts/ios/create_ios_universal.sh
+++ scripts/ios/create_ios_universal.sh
@@ -3,10 +3,8 @@
 # 2. libtool
 # See https://github.com/Co-Epi/app-backend-rust/wiki/Building-library-for-iOS
 
-cd ../..
-
 RUSTFLAGS="-Z embed-bitcode" cargo +ios-arm64 build --target aarch64-apple-ios --release --lib
-cargo build --target=x86_64-apple-ios
+cargo build --target=x86_64-apple-ios --release
 
 libtool -static -o ./target/libtcn_client.a ./target/aarch64-apple-ios/release/libtcn_client.a ./target/x86_64-apple-ios/release/libtcn_client.a
 
