be9ef648539caff2964d4e9dbdd7c02881efda36
Ivan Schuetz <ivanschuetz@gmail.com>
2020-05-24 18:20:59 +0200

    Add header

--- src/ios/c_headers/coepicore.h
+++ src/ios/c_headers/coepicore.h
@@ -3,3 +3,4 @@
 CFStringRef fetch_new_reports();
 CFStringRef get_reports(int32_t interval_number, int32_t interval_length);
 CFStringRef post_report(const char *to);
+CFStringRef submit_symptoms(const char *to);
