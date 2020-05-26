cd7975b7da72884c832bf94df270abee96715e53
Ivan Schütz <ivanschuetz@gmail.com>
2020-05-26 12:55:01 +0200

    Add header to set symptom ids

--- src/ios/c_headers/coepicore.h
+++ src/ios/c_headers/coepicore.h
@@ -7,8 +7,8 @@ CFStringRef post_report(const char *to);
 // Variant to send finished inputs as JSON. Not continuing this approach for now.
 // CFStringRef submit_symptoms_complete(const char *to);
 
+CFStringRef set_symptom_ids(const char *to);
 CFStringRef set_cough_type(const char *to);
-  
 CFStringRef set_cough_days(uint8_t is_set, uint32_t days);
 CFStringRef set_cough_status(const char *status);
 CFStringRef set_breathlessness_cause(const char *cause);
