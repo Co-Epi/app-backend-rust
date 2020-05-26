aa8ea97840ded0be557df1ccc039b40a32156bfc
Ivan Schütz <ivanschuetz@gmail.com>
2020-05-27 01:17:30 +0200

    Add contact time to alerts (#20)
    * Add contact time to alerts
    
    * Move to struct's methods, add unit test
    
    * Remove outdated tests
    
    * Remove comment

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,8 @@ fn u128_of_tcn(tcn: &TemporaryContactNumber) -> u128 {
 // }
 
 
+// TODO refactor these (byte_vec_to) convertions or better way?
+
 // TODO move to utils file or similar. Consider returning Result instead of panicking.
 pub fn byte_vec_to_16_byte_array(bytes: Vec<u8>) -> [u8; 16] {
   let mut array = [0; 16];
@@ -54,6 +56,21 @@ pub fn byte_vec_to_16_byte_array(bytes: Vec<u8>) -> [u8; 16] {
   array.copy_from_slice(bytes); 
   array
 }
+
+pub fn byte_vec_to_24_byte_array(bytes: Vec<u8>) -> [u8; 24] {
+  let mut array = [0; 24];
+  let bytes = &bytes[..array.len()]; // panics if not enough data
+  array.copy_from_slice(bytes); 
+  array
+}
+
+pub fn byte_vec_to_8_byte_array(bytes: Vec<u8>) -> [u8; 8] {
+  let mut array = [0; 8];
+  let bytes = &bytes[..array.len()]; // panics if not enough data
+  array.copy_from_slice(bytes); 
+  array
+}
+
 
 fn all_stored_tcns() -> Res<Vec<u128>> {
     let mut out: Vec<u128> = Vec::new();
@@ -88,7 +105,3 @@ fn all_stored_tcns() -> Res<Vec<u128>> {
 //     tx.prepare_commit()?.commit()?;
 //     Ok(())
 // }
-
-
-#[cfg(test)]
-mod is_it_working;
