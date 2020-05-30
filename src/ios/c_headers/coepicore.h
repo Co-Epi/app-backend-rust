7f1b5167c2a508ccc773a5a47f0a202117ad383c
Ivan Schuetz <ivanschuetz@gmail.com>
2020-05-30 11:30:36 +0200

    Use cbindgen

--- src/ios/c_headers/coepicore.h
+++ src/ios/c_headers/coepicore.h
@@ -1,26 +1,36 @@
 #include <CoreFoundation/CoreFoundation.h>
 
-CFStringRef bootstrap_core(const char *to);
 
-CFStringRef fetch_new_reports();
-CFStringRef record_tcn(const char *to);
+CFStringRef bootstrap_core(const char *db_path);
 
-CFStringRef get_reports(int32_t interval_number, int32_t interval_length);
-CFStringRef post_report(const char *to);
+CFStringRef clear_symptoms(void);
 
-// Variant to send finished inputs as JSON. Not continuing this approach for now.
-// CFStringRef submit_symptoms_complete(const char *to);
+CFStringRef fetch_new_reports(void);
 
-CFStringRef set_symptom_ids(const char *to);
-CFStringRef set_cough_type(const char *to);
-CFStringRef set_cough_days(uint8_t is_set, uint32_t days);
-CFStringRef set_cough_status(const char *status);
-CFStringRef set_breathlessness_cause(const char *cause);
-CFStringRef set_fever_days(uint8_t is_set, uint32_t days);
-CFStringRef set_fever_taken_temperature_today(uint8_t is_set, uint8_t taken);
-CFStringRef set_fever_taken_temperature_spot(const char *to);
-CFStringRef set_fever_highest_temperature_taken(uint8_t is_set, float days);
-CFStringRef set_earliest_symptom_started_days_ago(uint8_t is_set, uint32_t days);
+CFStringRef get_reports(uint32_t interval_number, uint32_t interval_length);
 
-CFStringRef submit_symptoms();
-CFStringRef clear_symptoms();
+CFStringRef post_report(const char *c_report);
+
+CFStringRef record_tcn(const char *c_tcn);
+
+CFStringRef set_breathlessness_cause(const char *c_cause);
+
+CFStringRef set_cough_days(uint8_t c_is_set, uint32_t c_days);
+
+CFStringRef set_cough_status(const char *c_status);
+
+CFStringRef set_cough_type(const char *c_cough_type);
+
+CFStringRef set_earliest_symptom_started_days_ago(uint8_t c_is_set, uint32_t c_days);
+
+CFStringRef set_fever_days(uint8_t c_is_set, uint32_t c_days);
+
+CFStringRef set_fever_highest_temperature_taken(uint8_t c_is_set, float c_temp);
+
+CFStringRef set_fever_taken_temperature_spot(const char *c_cause);
+
+CFStringRef set_fever_taken_temperature_today(uint8_t c_is_set, uint8_t c_taken);
+
+CFStringRef set_symptom_ids(const char *c_ids);
+
+CFStringRef submit_symptoms(void);
