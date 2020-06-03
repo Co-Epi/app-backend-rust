1a0ee9ecc48b52cc61e5f00c0aa8b29abd5f4328
duskoo <dusko.ojdanic@gmail.com>
2020-06-03 23:06:15 +0200

    Feature/submit inputs unit tests 2 (#30)
    * Implement public report <-> memo
    
    * Split in modules
    
    * Implement symptoms processing and keys management
    
    - FFI function to submit symptoms
    - Inputs form and mapping to public report
    - Storage of generated RAK and TCK (TCK not used for now, as TCNs not generated in lib yet) in preferences
    
    * [#14] SymptomInputs
    
    * [#14] test_public_report_with_inputs
    
    * [#14] test_public_report_should_be_sent
    
    * Typo fixed in TcnKeys: crate_report -> create_report
    
    * Compiling success
    
    * Cleanup
    
    * [#14] test_public_report_to_signed_report
    
    * [#14] test_submit_inputs
    
    * [#14] post-rebase compilation fixes
    
    * [#14] Cleanup and bit_list removal
    
    * [#14] start_index default is 1, added cfg(test)
    
    * BigArray trait generation for array size=66 only
    
    * PR comments addressed
    
    * refactoring: PreferencesMock -> PreferencesTckMock
    
    * Cleanup
    
    Co-authored-by: Ivan Schuetz <ivanschuetz@gmail.com>

--- src/preferences.rs
+++ src/preferences.rs
@@ -1,16 +1,39 @@
 use serde::{Serialize, Deserialize};
 use parking_lot::RwLock;
 use crate::reports_interval::ReportsInterval;
+use std::fmt;
+use std::option::Option;
+
+pub const TCK_SIZE_IN_BYTES: usize = 66; 
 
 pub enum PreferencesKey {
   LastCompletedReportsInterval
 }
 
+big_array! { BigArray; TCK_SIZE_IN_BYTES}
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 pub struct MyConfig {
   last_completed_reports_interval: Option<ReportsInterval>,
   autorization_key: Option<[u8; 32]>,
-  tck: Option<[u8; 32]>,
+  tck: Option<TckBytesWrapper>,
+}
+//Wrapper struct added to enable custom serialization of a large byte array
+#[derive(Copy, Clone, Serialize, Deserialize)]
+pub struct TckBytesWrapper {
+  #[serde(with = "BigArray")]
+  pub tck_bytes: [u8; TCK_SIZE_IN_BYTES]
+}
+
+impl fmt::Debug for TckBytesWrapper {
+  fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+      self.tck_bytes[..].fmt(formatter)
+  }
+}
+
+impl AsRef<[u8]> for TckBytesWrapper {
+  fn as_ref(&self) -> &[u8] {
+      &self.tck_bytes
+  }
 }
 
 impl Default for MyConfig {
@@ -31,8 +54,8 @@ pub trait Preferences {
   fn authorization_key(&self) -> Option<[u8; 32]>;
   fn set_autorization_key(&self, value: [u8; 32]);
 
-  fn tck(&self) -> Option<[u8; 32]>;
-  fn set_tck(&self, value: [u8; 32]);
+  fn tck(&self) -> Option<TckBytesWrapper>;
+  fn set_tck(&self, value: TckBytesWrapper);
 }
 
 pub struct PreferencesImpl {
@@ -73,11 +96,11 @@ impl Preferences for PreferencesImpl {
     }
   }
 
-  fn tck(&self) -> Option<[u8; 32]> {
+  fn tck(&self) -> Option<TckBytesWrapper> {
     self.config.read().tck
   }
 
-  fn set_tck(&self, value: [u8; 32]) {
+  fn set_tck(&self, value: TckBytesWrapper) {
     let mut config = self.config.write();
     config.tck = Some(value);
 
@@ -88,3 +111,37 @@ impl Preferences for PreferencesImpl {
     }
   }
 }
+
+pub struct PreferencesTckMock{
+    pub tck_bytes: TckBytesWrapper,
+}
+
+impl Preferences for PreferencesTckMock {
+  fn last_completed_reports_interval(&self, _: PreferencesKey) -> Option<ReportsInterval> { 
+    let reports_interval = ReportsInterval{number: 8899222, length: 12232 };
+    Option::Some(reports_interval)
+  }
+
+  fn set_last_completed_reports_interval(&self, _: PreferencesKey, _: ReportsInterval) { 
+    return;
+  }
+
+  fn authorization_key(&self) -> std::option::Option<[u8; 32]> { 
+    let bytes = [42, 118, 64, 131, 236, 36, 122, 23, 13, 108, 73, 171, 102, 145, 66, 91, 157, 105, 195, 126, 139, 162, 15, 31, 0, 22, 31, 230, 242, 241, 225, 85];
+    return Option::Some(bytes)
+  }
+
+  fn set_autorization_key(&self, _value: [u8; 32]) { 
+    return;
+  }
+
+  fn tck(&self) -> std::option::Option<TckBytesWrapper> { 
+    Some(self.tck_bytes)
+  }
+
+  fn set_tck(&self, _value: TckBytesWrapper) { 
+    return;
+  }
+
+}
+
--- src/tcn_ext/tcn_keys.rs
+++ src/tcn_ext/tcn_keys.rs
@@ -1,95 +1,182 @@
-use crate::preferences::Preferences;
-use tcn::{TemporaryContactKey, ReportAuthorizationKey, MemoType, SignedReport, Error, TemporaryContactNumber};
-use std::{cmp, io::Cursor, sync::Arc};
-use cmp::max;
+use crate::preferences::{Preferences, PreferencesTckMock, TckBytesWrapper, TCK_SIZE_IN_BYTES};
+use std::{io::Cursor, sync::Arc};
+use tcn::{Error, MemoType, ReportAuthorizationKey, SignedReport, TemporaryContactKey, TemporaryContactNumber};
 
 pub trait TcnKeys {
-  fn crate_report(&self, report: Vec<u8>) -> Result<SignedReport, Error>;
+  fn create_report(&self, report: Vec<u8>) -> Result<SignedReport, Error>;
   fn generate_tcn(&self) -> TemporaryContactNumber;
 }
 
-pub struct TcnKeysImpl<PreferencesType: Preferences> {
-  pub preferences: Arc<PreferencesType>,
+pub trait ReportAuthorizationKeyExt{
+    fn with_bytes(bytes: [u8;32]) -> ReportAuthorizationKey {
+        ReportAuthorizationKey::read(Cursor::new(&bytes)).expect("Couldn't read RAK bytes")
+    }
 }
 
-impl <PreferencesType: Preferences> TcnKeys for TcnKeysImpl<PreferencesType> {
-  fn crate_report(&self, report: Vec<u8>) -> Result<SignedReport, Error> {
-    let end_index = self.tck().index();
-    let minutes_in_14_days: u32 = 14 * 24 * 60 * 60;
-    let periods = minutes_in_14_days / 15;
-    let start_index = max(0, (end_index as u32) - periods) as u16;
+impl ReportAuthorizationKeyExt for ReportAuthorizationKey{}
 
-    self.rak().create_report(MemoType::CoEpiV1, report, start_index, end_index)
+pub trait TckBytesWrapperExt {
+  fn with_bytes(bytes: Vec<u8>) -> TckBytesWrapper {
+    let mut array = [0; TCK_SIZE_IN_BYTES];
+        let bytes = &bytes[..array.len()]; // panics if not enough data
+        array.copy_from_slice(bytes);
+        TckBytesWrapper { tck_bytes: array }
   }
+}
 
-  fn generate_tcn(&self) -> TemporaryContactNumber {
-    let tck = self.tck();
-    let tcn = tck.temporary_contact_number();
-    let new_tck = tck.ratchet();
+impl TckBytesWrapperExt for TckBytesWrapper{}
 
-    if let Some(new_tck) = new_tck {
-      self.set_tck(new_tck);
+pub struct TcnKeysImpl<PreferencesType: Preferences> {
+    pub preferences: Arc<PreferencesType>,
+}
+
+impl<PreferencesType: Preferences> TcnKeys for TcnKeysImpl<PreferencesType> {
+    fn create_report(&self, report: Vec<u8>) -> Result<SignedReport, Error> {
+        let end_index = self.tck().index();
+        let periods = 14 * 24 * (60 / 15);
+        let mut start_index = 1;
+        if end_index > periods {
+            start_index = (end_index - periods) as u16
+        }
+        println!("start_index={}, end_index={}", start_index, end_index);
+
+        self.rak()
+            .create_report(MemoType::CoEpiV1, report, start_index, end_index)
     }
 
-    println!("RUST generated tcn: {:?}", tcn);
-    // TODO: if None, rotate RAK
-    tcn
-  }
+    fn generate_tcn(&self) -> TemporaryContactNumber {
+      let tck = self.tck();
+      let tcn = tck.temporary_contact_number();
+      let new_tck = tck.ratchet();
+  
+      if let Some(new_tck) = new_tck {
+        self.set_tck(new_tck);
+      }
+  
+      println!("RUST generated tcn: {:?}", tcn);
+      // TODO: if None, rotate RAK
+      tcn
+    }
 }
 
-impl <PreferencesType: Preferences> TcnKeysImpl<PreferencesType> {
-  fn rak(&self) -> ReportAuthorizationKey {
-    self.preferences.authorization_key().map(|rak_bytes|
-      Self::bytes_to_rak(rak_bytes)
+impl<PreferencesType: Preferences> TcnKeysImpl<PreferencesType> {
+    fn rak(&self) -> ReportAuthorizationKey {
+        self.preferences
+            .authorization_key()
+            .map(|rak_bytes| ReportAuthorizationKey::with_bytes(rak_bytes))//Self::bytes_to_rak(rak_bytes))
+            .unwrap_or({
+                let new_key = ReportAuthorizationKey::new(rand::thread_rng());
+                self.preferences
+                    .set_autorization_key(Self::rak_to_bytes(new_key));
+                new_key
+            })
+    }
 
-    ).unwrap_or({
-      let new_key = ReportAuthorizationKey::new(rand::thread_rng());
-      self.preferences.set_autorization_key(Self::rak_to_bytes(new_key));
-      new_key
-    })
-  }
+    fn tck(&self) -> TemporaryContactKey {
+        self.preferences
+            .tck()
+            .map(|tck_bytes| Self::bytes_to_tck(tck_bytes))
+            .unwrap_or({ self.rak().initial_temporary_contact_key() })
+    }
 
-  fn tck(&self) -> TemporaryContactKey {
-    self.preferences.tck().map(|rak_bytes|
-      Self::bytes_to_tck(rak_bytes)
+    fn set_tck(&self, tck: TemporaryContactKey) {
+        self.preferences.set_tck(Self::tck_to_bytes(tck));
+    }
 
-    ).unwrap_or({
-      self.rak().initial_temporary_contact_key()
-    })
-  }
+    fn rak_to_bytes(rak: ReportAuthorizationKey) -> [u8; 32] {
+        let mut buf = Vec::new();
+        rak.write(Cursor::new(&mut buf))
+            .expect("Couldn't write RAK bytes");
+        Self::byte_vec_to_32_byte_array(buf)
+    }
 
-  fn set_tck(&self, tck: TemporaryContactKey) {
-    self.preferences.set_tck(Self::tck_to_bytes(tck));
-  }
+    fn byte_vec_to_32_byte_array(bytes: Vec<u8>) -> [u8; 32] {
+        let mut array = [0; 32];
+        let bytes = &bytes[..array.len()]; // panics if not enough data
+        array.copy_from_slice(bytes);
+        array
+    }
 
-  fn rak_to_bytes(rak: ReportAuthorizationKey) -> [u8; 32] {
-    let mut buf = Vec::new();
-    rak.write(Cursor::new(&mut buf))
-      .expect("Couldn't write RAK bytes");
-    Self::byte_vec_to_32_byte_array(buf)
-  }
+    pub fn tck_to_bytes(tck: TemporaryContactKey) -> TckBytesWrapper {
+        let mut buf = Vec::new();
+        tck.write(Cursor::new(&mut buf))
+            .expect("Couldn't write TCK bytes");
+        // Self::byte_vec_to_tck_byte_wrapper(buf)
+        TckBytesWrapper::with_bytes(buf)
+    }
 
-  fn byte_vec_to_32_byte_array(bytes: Vec<u8>) -> [u8; 32] {
-    let mut array = [0; 32];
-    let bytes = &bytes[..array.len()]; // panics if not enough data
-    array.copy_from_slice(bytes); 
-    array
-  }
+    fn bytes_to_tck(tck: TckBytesWrapper) -> TemporaryContactKey {
+        TemporaryContactKey::read(Cursor::new(&tck)).expect("Couldn't read TCK bytes")
+    }
+}
 
-  fn bytes_to_rak(bytes: [u8; 32]) -> ReportAuthorizationKey {
-    ReportAuthorizationKey::read(Cursor::new(&bytes))
-      .expect("Couldn't read RAK bytes")
-  }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-  fn tck_to_bytes(tck: TemporaryContactKey) -> [u8; 32] {
-    let mut buf = Vec::new();
-    tck.write(Cursor::new(&mut buf))
-      .expect("Couldn't write TCK bytes");
-    Self::byte_vec_to_32_byte_array(buf)
-  }
+    #[test]
+    fn test_rak() {
+        let new_key = ReportAuthorizationKey::new(rand::thread_rng());
+        let bytes = TcnKeysImpl::<PreferencesTckMock>::rak_to_bytes(new_key);
+        println!("{:?}", bytes);
+    }
 
-  fn bytes_to_tck(bytes: [u8; 32]) -> TemporaryContactKey {
-    TemporaryContactKey::read(Cursor::new(&bytes))
-      .expect("Couldn't read TCK bytes")
-  }
+    #[test]
+    fn test_load_rak() {
+        let bytes = [
+            42, 118, 64, 131, 236, 36, 122, 23, 13, 108, 73, 171, 102, 145, 66, 91, 157, 105, 195,
+            126, 139, 162, 15, 31, 0, 22, 31, 230, 242, 241, 225, 85,
+        ];
+        let key = ReportAuthorizationKey::with_bytes(bytes);
+        let tck = key.initial_temporary_contact_key();
+        let tck_bytes = TcnKeysImpl::<PreferencesTckMock>::tck_to_bytes(tck);
+
+    }
+
+    #[test]
+    fn test_load_tck() {
+        let rak_bytes = [
+            42, 118, 64, 131, 236, 36, 122, 23, 13, 108, 73, 171, 102, 145, 66, 91, 157, 105, 195,
+            126, 139, 162, 15, 31, 0, 22, 31, 230, 242, 241, 225, 85,
+        ];
+        let rak = ReportAuthorizationKey::with_bytes(rak_bytes);
+        let tck_1 = rak.initial_temporary_contact_key();
+
+        let tck_inner_bytes = [
+            34, 166, 47, 23, 224, 52, 240, 95, 140, 186, 95, 243, 26, 13, 174, 128, 224, 229, 158,
+            248, 117, 7, 118, 110, 108, 57, 67, 206, 129, 22, 84, 13,
+        ];
+        println!("count = {}", tck_inner_bytes.len());
+
+        let version_bytes: [u8; 2] = [1, 0];
+
+        let version_vec = version_bytes.to_vec();
+        let rak_vec = rak_bytes.to_vec();
+        let tck_inner_vec = tck_inner_bytes.to_vec();
+
+        let complete_tck_vec = [&version_vec[..], &rak_vec[..], &tck_inner_vec[..]].concat();
+
+        let tck_bytes_wrapped = TckBytesWrapper::with_bytes(complete_tck_vec);
+        let tck = TcnKeysImpl::<PreferencesTckMock>::bytes_to_tck(tck_bytes_wrapped);
+
+        println!("{:#?}", tck);
+        
+    }
+
+    #[test]
+    fn test_generate_tcns() {
+        let rak_bytes = [
+            42, 118, 64, 131, 236, 36, 122, 23, 13, 108, 73, 171, 102, 145, 66, 91, 157, 105, 195,
+            126, 139, 162, 15, 31, 0, 22, 31, 230, 242, 241, 225, 85,
+        ];
+
+        let rak = ReportAuthorizationKey::with_bytes(rak_bytes);
+        let mut tck = rak.initial_temporary_contact_key(); // tck <- tck_1
+        let mut tcns = Vec::new();
+
+        for _ in 0..100 {
+            tcns.push(tck.temporary_contact_number());
+            tck = tck.ratchet().unwrap();
+        }
+    }
 }
