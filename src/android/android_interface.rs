7e8e1ff427ea9b535ab5eb58d7d097a61152445c
Ivan Schuetz <ivanschuetz@gmail.com>
2020-06-12 16:43:00 +0200

    Autoformat

--- src/android/android_interface.rs
+++ src/android/android_interface.rs
@@ -1,16 +1,17 @@
-use std::os::raw::{c_char};
-use std::ffi::{CString, CStr};
-
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
+pub extern "C" fn rust_greeting(to: *const c_char) -> *mut c_char {
     let c_str = unsafe { CStr::from_ptr(to) };
     let recipient = match c_str.to_str() {
         Err(_) => "there",
         Ok(string) => string,
     };
 
-    CString::new("Hello ".to_owned() + recipient).unwrap().into_raw()
+    CString::new("Hello ".to_owned() + recipient)
+        .unwrap()
+        .into_raw()
 }
 
 /// Expose the JNI interface for android below
@@ -18,31 +19,50 @@ pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
 pub mod android {
     extern crate jni;
 
-    use super::*;
-    use self::jni::JNIEnv;
     use self::jni::objects::{JClass, JString};
-    use self::jni::sys::{jstring};
+    use self::jni::sys::jstring;
+    use self::jni::JNIEnv;
+    use super::*;
 
     #[no_mangle]
-    pub unsafe extern fn Java_org_coepi_android_api_NativeApi_testSendReceiveString(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
+    pub unsafe extern "C" fn Java_org_coepi_android_api_NativeApi_testSendReceiveString(
+        env: JNIEnv,
+        _: JClass,
+        java_pattern: JString,
+    ) -> jstring {
         // Our Java companion code might pass-in "world" as a string, hence the name.
-        let world = rust_greeting(env.get_string(java_pattern).expect("invalid pattern string").as_ptr());
+        let world = rust_greeting(
+            env.get_string(java_pattern)
+                .expect("invalid pattern string")
+                .as_ptr(),
+        );
         // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
         let world_ptr = CString::from_raw(world);
-        let output = env.new_string(world_ptr.to_str().unwrap()).expect("Couldn't create java string!");
+        let output = env
+            .new_string(world_ptr.to_str().unwrap())
+            .expect("Couldn't create java string!");
 
         output.into_inner()
     }
 
     #[no_mangle]
-    pub unsafe extern fn Java_org_coepi_android_api_NativeApi_bootstrapCore(env: JNIEnv, _: JClass, java_db_path: JString) -> jstring {
+    pub unsafe extern "C" fn Java_org_coepi_android_api_NativeApi_bootstrapCore(
+        env: JNIEnv,
+        _: JClass,
+        java_db_path: JString,
+    ) -> jstring {
         // Our Java companion code might pass-in "world" as a string, hence the name.
-        let world = rust_greeting(env.get_string(java_db_path).expect("invalid db_path string").as_ptr());
+        let world = rust_greeting(
+            env.get_string(java_db_path)
+                .expect("invalid db_path string")
+                .as_ptr(),
+        );
         // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
         let world_ptr = CString::from_raw(world);
-        let output = env.new_string(world_ptr.to_str().unwrap()).expect("Couldn't create java string!");
+        let output = env
+            .new_string(world_ptr.to_str().unwrap())
+            .expect("Couldn't create java string!");
 
         output.into_inner()
     }
-    
 }
