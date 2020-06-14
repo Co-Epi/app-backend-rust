13bec7e747671216294b63ee35d0b8f5cd739fb7
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2020-06-14 15:27:23 +0200

    Using Boxed logger

--- src/simple_logger.rs
+++ src/simple_logger.rs
@@ -5,33 +5,77 @@ use chrono::{Local, Utc};
 use log::*;
 use std::sync::Once;
 
-static LOGGER: SimpleLogger = SimpleLogger;
+// #[cfg(feature = "std")]
+// use log::set_boxed_logger;
+
+static LOGGER: SimpleLogger = SimpleLogger {
+    coepi_specific_logs_only: false,
+};
+
+static LOGGER_COEPI: SimpleLogger = SimpleLogger {
+    coepi_specific_logs_only: true,
+};
+
 static INIT: Once = Once::new();
 
-pub fn init() -> Result<(), SetLoggerError> {
-    log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Trace))
-}
-
-pub fn setup_with_level(level: LevelFilter){
-    // Guaranteed to be executed only once (even if called multiple times).
+pub fn setup_with_level_and_target(level: LevelFilter, coepi_only: bool) {
     INIT.call_once(|| {
         println!("RUST : Logger level : {}", level);
-        log::set_logger(&LOGGER).map(|()| log::set_max_level(level)).expect("Logger initialization failed!");
-
+        if coepi_only {
+            log::set_logger(&LOGGER_COEPI)
+                .map(|()| log::set_max_level(level))
+                .expect("Logger initialization failed!");
+        } else {
+            log::set_logger(&LOGGER)
+                .map(|()| log::set_max_level(level))
+                .expect("Logger initialization failed!");
+        }
     });
-
-    // let resulting_level = log::max_level();
-    // info!("Resulting level : {}", log::max_level());
-    // println!("STATIC_MAX_LEVEL : {}", log::STATIC_MAX_LEVEL);
-    // info!("Trace log level enabled: {}", log_enabled!(Level::Trace));
-    // info!("Debug log level enabled: {}", log_enabled!(Level::Debug));
 }
 
-pub fn setup(){
+//Boxed logger setup
+pub fn setup_boxed(level: LevelFilter, coepi_only: bool) {
+    INIT.call_once(|| {
+        println!("RUST : Logger level : {}", level);
+        if coepi_only {
+            println!("RUST : CoEpi Logs Only");
+            set_boxed_logger(Box::new(SimpleLogger {
+                coepi_specific_logs_only: true,
+            }))
+            .map(|()| log::set_max_level(level))
+            .expect("Logger initialization failed!");
+        } else {
+            set_boxed_logger(Box::new(SimpleLogger {
+                coepi_specific_logs_only: false,
+            }))
+            .map(|()| log::set_max_level(level))
+            .expect("Logger initialization failed!");
+        }
+    })
+}
+//https://github.com/rust-lang/log/blob/efcc39c5217edae4f481b73357ca2f868bfe0a2c/test_max_level_features/main.rs#L10
+fn set_boxed_logger(logger: Box<Log>) -> Result<(), log::SetLoggerError> {
+    log::set_logger(Box::leak(logger))
+}
+
+pub fn setup_with_level(level: LevelFilter) {
+    // Guaranteed to be executed only once (even if called multiple times).
+
+    INIT.call_once(|| {
+        println!("RUST : Logger level : {}", level);
+        log::set_logger(&LOGGER)
+            .map(|()| log::set_max_level(level))
+            .expect("Logger initialization failed!");
+    });
+}
+
+pub fn setup() {
     setup_with_level(LevelFilter::Trace);
 }
 
-pub struct SimpleLogger;
+pub struct SimpleLogger {
+    coepi_specific_logs_only: bool,
+}
 
 #[cfg(not(test))]
 impl SimpleLogger {
@@ -65,8 +109,6 @@ impl log::Log for SimpleLogger {
                 Level::Trace => CoreLogLevel::Trace,
             };
 
-            //TODO: compare levels and continue only if required
-
             let lmts = CoreLogMessageThreadSafe {
                 level: lvl,
                 text: arg_string,
@@ -85,12 +127,31 @@ impl log::Log for SimpleLogger {
 impl log::Log for SimpleLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
         // println!("metadata level : {}", metadata.level());
-        metadata.level() <= log::max_level()
+        // println!("metadata target : {}", metadata.target());
+        // let tgt = metadata.target();
+        // let mtc = tgt.starts_with("coepi_core::");
+        // println!("Match: {}", mtc);
+        let level_threshhold = metadata.level() <= log::max_level();
+
+        // let sl = SimpleLogger::from(logger());
+
+        if (self.coepi_specific_logs_only) {
+            level_threshhold && metadata.target().starts_with("coepi_core::")
+        } else {
+            level_threshhold
+        }
     }
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            println!("{} {} {}:{} - {}", Local::now().format("%H:%M:%S.%s"), record.level(), record.target(), record.line().unwrap_or(0), record.args());          
+            println!(
+                "{} {} {}:{} - {}",
+                Local::now().format("%H:%M:%S.%s"),
+                record.level(),
+                record.target(),
+                record.line().unwrap_or(0),
+                record.args()
+            );
         }
     }
 
@@ -101,17 +162,13 @@ use crate::simple_logger;
 
 #[test]
 fn verify_test_macros() {
-    // std::env::set_var("RUST_LOG", "trace");
     simple_logger::setup_with_level(LevelFilter::Debug);
     println!("Resulting level : {}", log::max_level());
     println!("STATIC_MAX_LEVEL : {}", log::STATIC_MAX_LEVEL);
-    info!("first line");
     trace!("trace");
-    // log::log!(Level::Trace, "Uja trace");
-    // log::log!(Level::Debug, "Uja debug");
     debug!("debug");
     info!("info");
     warn!("warn");
     error!("error");
-    assert_eq!(1,1)
+    assert_eq!(1, 1)
 }
