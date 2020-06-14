457b2ee230042e71e28743e02cc27e0a6a5ef0d1
Ivan Schuetz <ivanschuetz@gmail.com>
2020-06-14 18:58:29 +0200

    Merge build and linking scripts

--- scripts/android/create_android.sh
+++ scripts/android/create_android.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Building  ###########################################################
+
 # See https://github.com/Co-Epi/app-backend-rust/wiki/Building-library-for-Android
 
 # debug builds
@@ -11,4 +13,24 @@ cargo ndk --platform 29 --target armv7-linux-androideabi build
 # cargo ndk --platform 29 --target x86_64-linux-android build --release
 # cargo ndk --platform 29 --target aarch64-linux-android build --release
 # cargo ndk --platform 29 --target armv7-linux-androideabi build --release
+
+# Linking ###########################################################
+
+PATH_TO_LIB_REPO="."
+PATH_TO_ANDROID_REPO="<insert path>"
+
+echo "Copying library files to Android app: ${PATH_TO_ANDROID_REPO}"
+
+PATH_TO_ANDROID_MAIN=$PATH_TO_ANDROID_REPO/app/src/main
+
+mkdir $PATH_TO_ANDROID_MAIN/jniLibs
+mkdir $PATH_TO_ANDROID_MAIN/jniLibs/arm64
+mkdir $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a
+mkdir $PATH_TO_ANDROID_MAIN/jniLibs/armeabi
+mkdir $PATH_TO_ANDROID_MAIN/jniLibs/x86
+
+ln -s $PATH_TO_LIB_REPO/target/aarch64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64/libcoepi_core.so
+ln -s $PATH_TO_LIB_REPO/target/aarch64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a/libcoepi_core.so
+ln -s $PATH_TO_LIB_REPO/target/i686-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86/libcoepi_core.so
+ln -s $PATH_TO_LIB_REPO/target/armv7-linux-androideabi/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/armeabi/libcoepi_core.so
 
