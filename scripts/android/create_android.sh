8b423a9745dbf0ec57193c6b1f5e247b258a79c7
Ivan Schuetz <ivanschuetz@gmail.com>
2020-06-14 18:26:56 +0200

    Use cargo ndk

--- scripts/android/create_android.sh
+++ scripts/android/create_android.sh
@@ -2,7 +2,13 @@
 
 # See https://github.com/Co-Epi/app-backend-rust/wiki/Building-library-for-Android
 
-cargo build --target=aarch64-linux-android --release
-cargo build --target=armv7-linux-androideabi --release
-cargo build --target=i686-linux-android --release
+# debug builds
+cargo ndk --platform 29 --target x86_64-linux-android build
+cargo ndk --platform 29 --target aarch64-linux-android build
+cargo ndk --platform 29 --target armv7-linux-androideabi build
+
+# release builds
+# cargo ndk --platform 29 --target x86_64-linux-android build --release
+# cargo ndk --platform 29 --target aarch64-linux-android build --release
+# cargo ndk --platform 29 --target armv7-linux-androideabi build --release
 
