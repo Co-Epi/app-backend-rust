6372db684ff2f9d545426bda8374bd8c4eb9523f
Ivan Schuetz <ivanschuetz@gmail.com>
2020-06-17 00:09:57 +0200

    Support 32 bit Android emulator

--- scripts/android/create_android.sh
+++ scripts/android/create_android.sh
@@ -21,11 +21,13 @@ fi
 cargo ndk --platform 29 --target x86_64-linux-android build
 cargo ndk --platform 29 --target aarch64-linux-android build
 cargo ndk --platform 29 --target armv7-linux-androideabi build
+cargo ndk --platform 29 --target i686-linux-android build
 
 # release builds
 # cargo ndk --platform 29 --target x86_64-linux-android build --release
 # cargo ndk --platform 29 --target aarch64-linux-android build --release
 # cargo ndk --platform 29 --target armv7-linux-androideabi build --release
+# cargo ndk --platform 29 --target i686-linux-android build --release
 
 # Linking ###########################################################
 
@@ -40,15 +42,18 @@ mkdir $PATH_TO_ANDROID_MAIN/jniLibs/arm64
 mkdir $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a
 mkdir $PATH_TO_ANDROID_MAIN/jniLibs/armeabi
 mkdir $PATH_TO_ANDROID_MAIN/jniLibs/x86_64
+mkdir $PATH_TO_ANDROID_MAIN/jniLibs/x86
 
 # debug
 ln -s $(pwd)/target/aarch64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64/libcoepi_core.so
 ln -s $(pwd)/target/aarch64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a/libcoepi_core.so
 ln -s $(pwd)/target/x86_64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86_64/libcoepi_core.so
 ln -s $(pwd)/target/armv7-linux-androideabi/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/armeabi/libcoepi_core.so
+ln -s $(pwd)/target/i686-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86/libcoepi_core.so
 
 # release
 # ln -s $(pwd)/target/aarch64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64/libcoepi_core.so
 # ln -s $(pwd)/target/aarch64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a/libcoepi_core.so
 # ln -s $(pwd)/target/x86_64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86_64/libcoepi_core.so
 # ln -s $(pwd)/target/armv7-linux-androideabi/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/armeabi/libcoepi_core.so
+# ln -s $(pwd)/target/i686-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86/libcoepi_core.so
