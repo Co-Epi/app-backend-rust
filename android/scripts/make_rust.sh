02a65f3a2dc275cd8246d35987862be25c2ac9f9
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2020-06-29 01:59:45 +0200

    [#103] added env var PATH_TO_ANDROID_REPO

--- android/scripts/make_rust.sh
+++ android/scripts/make_rust.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
 # Run from repo's root folder (relative to Gradle build file. TODO do this in Gradle)
-cd ../..
-echo "Running make_rust.sh from $(pwd)"
+# cd ../..
+
+if [ -d "android/core/gradle" ]; then
+    echo "Starting..."
+else
+    echo "Error: this script must be run in project root"
+    exit 5
+fi
+
+root=$(pwd)
+
+echo "Running make_rust.sh from $root"
 
 # Building  ###########################################################
 
 # See https://github.com/Co-Epi/app-backend-rust/wiki/Building-library-for-Android
+
+if test -z "$PATH_TO_ANDROID_REPO"; then
+    echo "Environment variable PATH_TO_ANDROID_REPO not set. Please set the variable using: 'export PATH_TO_ANDROID_REPO=<your_path_here>'"
+    exit 6
+else
+    echo "\$PATH_TO_ANDROID_REPO is $PATH_TO_ANDROID_REPO"
+    if [ -d "$PATH_TO_ANDROID_REPO" ]; then
+        echo "Will copy files to ${PATH_TO_ANDROID_REPO}..."
+    else
+        echo "Error: ${PATH_TO_ANDROID_REPO} not found. Make sure that you have checked out the iOS project and that the path specified in PATH_TO_ANDROID_REPO environment variable is correct"
+        exit 7
+    fi
+fi
+
+cd android/core || exit 1
 
 if [[ $* == *--release* ]]; then
     # release builds
@@ -22,13 +47,14 @@ else
     cargo ndk --platform 29 --target i686-linux-android build
 fi
 
+cd ../.. || exit 2
 
 # Linking ###########################################################
 
-echo "Copying .so files to Android project"
+#PATH_TO_ANDROID_LIBRARY="./android/core"
+PATH_TO_ANDROID_MAIN=$root/android/core/core/src/main
 
-PATH_TO_ANDROID_LIBRARY="./android/core"
-PATH_TO_ANDROID_MAIN=$PATH_TO_ANDROID_LIBRARY/core/src/main
+echo "PATH_TO_ANDROID_MAIN is $PATH_TO_ANDROID_MAIN"
 
 rm -fr $PATH_TO_ANDROID_MAIN/jniLibs
 mkdir $PATH_TO_ANDROID_MAIN/jniLibs
@@ -39,14 +65,23 @@ mkdir $PATH_TO_ANDROID_MAIN/jniLibs/x86
 
 if [[ $* == *--release* ]]; then
     # release
-    cp $(pwd)/target/aarch64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a/libcoepi_core.so
-    cp $(pwd)/target/x86_64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86_64/libcoepi_core.so
-    cp $(pwd)/target/armv7-linux-androideabi/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/armeabi/libcoepi_core.so
-    cp $(pwd)/target/i686-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86/libcoepi_core.so
+    cp $root/target/aarch64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a/libcoepi_core.so
+    cp $root/target/x86_64-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86_64/libcoepi_core.so
+    cp $root/target/armv7-linux-androideabi/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/armeabi/libcoepi_core.so
+    cp $root/target/i686-linux-android/release/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86/libcoepi_core.so
 else
     # debug
-    cp $(pwd)/target/aarch64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a/libcoepi_core.so
-    cp $(pwd)/target/x86_64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86_64/libcoepi_core.so
-    cp $(pwd)/target/armv7-linux-androideabi/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/armeabi/libcoepi_core.so
-    cp $(pwd)/target/i686-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86/libcoepi_core.so
+    echo "Copying .so files to $PATH_TO_ANDROID_MAIN/jniLibs..."
+    cp $root/target/aarch64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/arm64-v8a/libcoepi_core.so
+    cp $root/target/x86_64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86_64/libcoepi_core.so
+    cp $root/target/armv7-linux-androideabi/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/armeabi/libcoepi_core.so
+    cp $root/target/i686-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_MAIN/jniLibs/x86/libcoepi_core.so
+
+    echo "Copying .so files to  $PATH_TO_ANDROID_REPO/app/src/main/jniLibs..."
+    cp $root/target/aarch64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_REPO/app/src/main/jniLibs/arm64-v8a/libcoepi_core.so
+    cp $root/target/x86_64-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_REPO/app/src/main/jniLibs/x86_64/libcoepi_core.so
+    cp $root/target/armv7-linux-androideabi/debug/libcoepi_core.so $PATH_TO_ANDROID_REPO/app/src/main/jniLibs/armeabi/libcoepi_core.so
+    cp $root/target/i686-linux-android/debug/libcoepi_core.so $PATH_TO_ANDROID_REPO/app/src/main/jniLibs/x86/libcoepi_core.so
 fi
+
+echo "Done"
