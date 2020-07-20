9131deddfe7cc188074f33d0cd90e5ce966779a1
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2020-07-20 11:35:10 +0200

    Anchor logic to script parent folder using BASH_SOURCE

--- android/scripts/make_rust.sh
+++ android/scripts/make_rust.sh
@@ -1,34 +1,27 @@
-#!/bin/bash
+#!/bin/bash 
+
+# to debug the script add " -x" to the shebang. Ie. "#!/bin/bash -x"
+
+# get script folder
+folder=$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P )
+
+echo "$folder"
+
+# get android main folder relative to script folder
+parent="$(dirname "$folder")"
+PATH_TO_ANDROID_MAIN="$parent/core/core/src/main"
+echo "PATH_TO_ANDROID_MAIN is $PATH_TO_ANDROID_MAIN"
+
+# get project root
+root="$(dirname "$parent")"
+echo "Root folder: $root"
+
 
 # Run from repo's root folder (relative to Gradle build file. TODO do this in Gradle)
-
-if [ -d "android/core/gradle" ]; then
-    echo "Starting..."
-else
-    echo "Error: this script must be run in project root"
-    exit 5
-fi
-
-root=$(pwd)
-
-echo "Running make_rust.sh from $root"
 
 # Building  ###########################################################
 
 # See https://github.com/Co-Epi/app-backend-rust/wiki/Building-library-for-Android
-
-if test -z "$PATH_TO_ANDROID_REPO"; then
-    echo "Environment variable PATH_TO_ANDROID_REPO not set. Please set the variable using: 'export PATH_TO_ANDROID_REPO=<your_path_here>'"
-    exit 6
-else
-    echo "\$PATH_TO_ANDROID_REPO is $PATH_TO_ANDROID_REPO"
-    if [ -d "$PATH_TO_ANDROID_REPO" ]; then
-        echo "Will copy files to ${PATH_TO_ANDROID_REPO}..."
-    else
-        echo "Error: ${PATH_TO_ANDROID_REPO} not found. Make sure that you have checked out the corresponding Android project and that the path specified in PATH_TO_ANDROID_REPO environment variable is correct"
-        exit 7
-    fi
-fi
 
 # Order of target_triples has to match the order of arhitectures:
 # aarch64-linux-android --> arm64-v8a
@@ -60,10 +53,6 @@ done
 
 # Linking ###########################################################
 
-PATH_TO_ANDROID_MAIN=$root/android/core/core/src/main
-
-echo "PATH_TO_ANDROID_MAIN is $PATH_TO_ANDROID_MAIN"
-
 rm -fr $PATH_TO_ANDROID_MAIN/jniLibs
 mkdir $PATH_TO_ANDROID_MAIN/jniLibs
 for arch in ${architectures[@]}; do
@@ -80,11 +69,6 @@ limit=$(($cnt - 1))
 i=0
 while [ "$i" -le "$limit" ]; do
     cp $root/target/${target_triples[i]}/$build_type/$lib_file $PATH_TO_ANDROID_MAIN/jniLibs/${architectures[i]}/$lib_file
-
-    if [ $build_type = debug ]; then
-        echo "Copying $build_type/$lib_file to  $PATH_TO_ANDROID_REPO/app/src/main/jniLibs/${architectures[i]}/"
-        cp $root/target/${target_triples[i]}/$build_type/$lib_file $PATH_TO_ANDROID_REPO/app/src/main/jniLibs/${architectures[i]}/$lib_file
-    fi
     i=$(($i + 1))
 done
 
